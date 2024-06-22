152bcb4a3bd1268659fa9c278d35ce9c5b5c60d4
kumaranil3921 <kumaranil3921@gmail.com>
2024-06-22 20:36:08 +0530

    Game Dev Completed

---
+++ README.md
@@ -0,0 +1,3 @@
+# Tic Tac Toe
+
+<a href="https://kumaranil3921.github.io/Tic-Tac-Toe/" target="_blank">Demo</a>
---
+++ src/Components/Player.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
+  }
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
+  if (isEditing) {
+    editablePlayerName = (
+      <input
+        type="text"
+        placeholder="Enter Name"
+        required
+        value={playerName}
+        onChange={handleChange}
+      />
+    );
+    btnCaption = "Save";
+  }
+  return (
+    <li className={isActive ? "active" : ""}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{btnCaption}</button>
+    </li>
+  );
+}
